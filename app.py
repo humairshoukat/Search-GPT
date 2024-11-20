cbddb9b4a61aea28df50a1b07fd7e0c3e20dd723
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2024-11-20 14:58:44 +0500

    update: app.py

--- app.py
+++ app.py
@@ -11,24 +11,20 @@ load_dotenv()
 st.set_page_config(page_title="Search GPT", page_icon="🔍")
 
 # UI for the app
-st.title("Search GPT")
+st.title("Search GPT 🔍")
 st.write(
     "Chat GPT with real-time internet search capabilities. "
 )
-
-# If we want to add a line break between two lines then use st.markdown() instead st.write()
-# st.markdown(
-#     "Chat GPT with real-time internet search capabilities.  \n"
-#     "Built using Python, LangChain, Streamlit, OpenAI, and Google Search Results."
-# )
 
 # Add spacing with st.write() OR with custom HTML & CSS using st.markdown()
 # st.write("")  # Empty string for line space/gap
 st.markdown("<div style='margin-top: 25px;'></div>", unsafe_allow_html=True)
 
+prompt = "Please answer this question in detail: "
 
 # Get user input
-prompt = st.text_input("Ask your question here...")
+prompt += st.text_input("Ask your question here...")
+button = st.button("Search")
 
 # Initialize LLM
 llm = OpenAI(temperature=0.7, openai_api_key=os.getenv('OPENAI_API_KEY'))
@@ -41,6 +37,10 @@ tools = load_tools(tool_names)
 agent = initialize_agent(tools, llm, agent='zero-shot-react-description', verbose=True)
 
 # Process user input
-if prompt:
-    response = agent.run(prompt)
-    st.write(response)
+if button:
+    with st.spinner("Searching..."):
+        try:
+            response = agent.run(prompt)
+            st.write(response)
+        except Exception as e:
+            st.write("An error occured")
